604186b17c96f87ef6bfecb97abdc5096b18d358
Luka <luka.luka.luka93@gmail.com>
2025-01-23 15:17:16 -0800

    i set the new state in the handleselectsquare funcion

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -10,7 +10,11 @@ export default function GameBoard (){
     const [ gameBoard, setGameBoard ] = useState(initialGameBoard);
 
     function handleSelectSquare(rowIndex, colIndex){
-        
+        setGameBoard((prevGameBoard)=> {
+            const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+            updatedBoard[rowIndex][colIndex] = 'X';
+            return updatedBoard;
+        } )
     }
 
 
