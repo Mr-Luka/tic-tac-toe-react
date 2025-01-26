dfb9080d0afe20da67471a6f121ed5aefadb5cad
Luka <luka.luka.luka93@gmail.com>
2025-01-26 14:39:16 -0800

    adding more comments to the project

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -8,11 +8,12 @@ const initialGameBoard = [
 // making the board
 
 export default function GameBoard (){
-    const [ gameBoard, setGameBoard ] = useState(initialGameBoard);
+    const [ gameBoard, setGameBoard ] = useState(initialGameBoard);// here we set the initialGameBoard as a initial state a.k.a. gameBoard
 
     function handleSelectSquare(rowIndex, colIndex){
-        setGameBoard((prevGameBoard)=> {
-            const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+        setGameBoard((prevGameBoard)=> { // function form of updating the state by passing a function to the state updating function by passing a function to the state updating function, we will put our prevGameBoard as an argument here passed in automatically by React
+            const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])]; // updating the state in an immutable way
+//  technically a new array object in memory that contains the old array elements as child elements
             updatedBoard[rowIndex][colIndex] = 'X';
             return updatedBoard;
         } )
