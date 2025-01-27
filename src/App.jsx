2eb6140b811ffe0545dbfe46bc0570fc758cf5a7
Luka <luka.luka.luka93@gmail.com>
2025-01-27 14:31:26 -0800

    made another component Log.jsx and made another useState for gameTurns and setGameTurns, where we are gonna store the data about which player has played, and I commented out in GameBoard the previous state and function

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import {useState} from 'react';
 import Player from './components/Player.jsx';
 import GameBoard from './components/GameBoard.jsx';
+import Log from './components/Log.jsx';
 
 
 function App() {
+  const [gameTurns, setGameTurns] = useState( [] );
   const [activePlayer, setActivePlayer] = useState('X');
 
   function handleSelectSquare() {
@@ -20,7 +22,7 @@ function App() {
       </ol>
         <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer}/>
       </div>
-      LOG
+      <Log />
     </main>
   )
 }
