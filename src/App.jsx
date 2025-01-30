d9a8bf1f6ac201a7bb478d221f1e6144c9bf78bf
Luka <luka.luka.luka93@gmail.com>
2025-01-30 13:21:35 -0800

    made a condition to check if there is a draw, and in gameOver.jsx we also made a condition to check if its a draw or a winner

--- src/App.jsx
+++ src/App.jsx
@@ -50,6 +50,8 @@ function App() {
       }
     }
 
+    const hasDraw = gameTurns.length === 9 && !winner; // we have a draw, no winner
+
   function handleSelectSquare(rowIndex, colIndex) {
     // setActivePlayer((curActivePlayer) => (curActivePlayer === 'X' ? 'O' : 'X'));
     setGameTurns ((prevTurns) => {
@@ -74,7 +76,7 @@ function App() {
           symbol="O" 
           isActive={activePlayer === 'O'}/>
       </ol>
-      {winner && <GameOver winner={winner} />}
+      {(winner || hasDraw) && <GameOver winner={winner} />}
         <GameBoard 
           onSelectSquare={handleSelectSquare} 
           board={gameBoard}/>
