b37615ae881456e3f245b3c8ca85cc70243cad16
Luka <luka.luka.luka93@gmail.com>
2025-02-03 17:56:00 -0800

    more comments and notes in LOG.jsx

--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -1,6 +1,4 @@
-
 export default function Log({turns}){
-
     return <ol id="log">
             {turns.map(turn => (
                 <li key={`${turn.square.row}${turn.square.col}`}>
@@ -9,3 +7,13 @@ export default function Log({turns}){
                 ))}
            </ol>
 }
+/*
+Log component that will map through turns and for every turn it will generate a listed item, 
+and each listed item will generate name of the player by pulling the data from the parent component, 
+the square that the player selected by pulling the data from the parent component for the row and column.
+App() - lifting the computed value up.
+
+we destructured turns as a parameter, because we are gonna use the same data in the App()
+
+
+*/
