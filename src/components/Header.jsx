4387a0876d5563e1292bcd598652bb232765ba7c
Luka <luka.luka.luka93@gmail.com>
2025-02-14 10:43:34 -0800

    restarting the project

--- src/components/Header.jsx
+++
@@ -1,8 +0,0 @@
-import logo from './game-logo.png'
-
-export default function Header () {
-    return <header>
-        <img src={logo} alt="handrawn tik tak toe"/>
-        <h1>Tik-Tak-Toe</h1>
-    </header>
-}
