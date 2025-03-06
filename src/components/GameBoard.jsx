f675daa5ebc18bacf1c54569e32a6399619069a5
Luka <luka.luka.luka93@gmail.com>
2025-03-06 13:16:12 -0800

    made board game with X symbol only

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,16 +1,39 @@
+import {useState} from 'react';
 
-
-const InitialGameBoard = [
+const initialGameBoard = [
     [null, null, null],
     [null, null, null],
     [null, null, null]
 ]
 
 export default function GameBoard(){
+    const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+    function handleSquareClick(rowIndex, colIndex){
+        setGameBoard(prevGameBoard => {
+            const newGameBoard = [...prevGameBoard.map(newArray => [...newArray])];
+            newGameBoard[rowIndex][colIndex] = 'X';
+            return newGameBoard;
+        })
+    }
+
+
 
    return (
         <ol id="game-board">
-          
+          {gameBoard.map((row, rowIndex)=> 
+            <li key={rowIndex}>
+                <ol>
+                    {row.map((playerSymbol, colIndex)=>
+                     <li key={colIndex}>
+                        <button
+                           onClick={()=> handleSquareClick(rowIndex, colIndex)}
+                        >
+                          {playerSymbol}</button>
+                     </li>)}
+                </ol>
+            </li>)}
         </ol>
     )
 }
+
