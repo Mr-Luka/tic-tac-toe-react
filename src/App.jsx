720c85655bc364455921272485acfc91b030fb9e
Luka <luka.luka.luka93@gmail.com>
2025-03-11 12:42:34 -0700

    finished with players component using ref

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,22 @@
-import {useState} from 'react';
-
 import Header from './components/Header.jsx';
 import Player from './components/Player.jsx';
 import GameBoard from './components/GameBoard.jsx';
-import Log from './components/Log.jsx';
-import {WINNING_COMBINATIONS} from './components/winning-combinations.js';
-import GameOver from './components/GameOver.jsx';
-
-
 
 function App() {
 
   return (
     <main>
-
+      <Header />
       <div id="game-container">
         <ol id="players" className="highlight-player"> 
-
+          <Player  
+            name= 'Player 1'
+          />
+          <Player 
+            name= 'Player 2'
+          />
         </ol>
-
+        <GameBoard />
       </div>
 
     </main>
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,12 +1,28 @@
+import {useState, useRef} from 'react';
 
+export default function Player({name}){
+    const playerName = useRef(name)
+    const [isEditing, setIsEditing] = useState(false);
 
-export default function Player(){
+    function handleClick(){
+        setIsEditing(isEditing => !isEditing);
+
+        if(isEditing){
+            playerName.current = playerName.current.value;
+        }
+    }
+
+    let editableName = <span className="player-name">{playerName.current}</span>
+    if(isEditing){
+        editableName = <input type='text' required ref={playerName} defaultValue={playerName.current} />
+    }
+
 
     return <li>
             <span className="player">
-            <span className="player-name">Player</span>
+            {editableName}
             <span className="player-symbol">X</span>
-            <button>Save</button>
+            <button onClick={handleClick}>{isEditing ? 'Save' : 'Edit'}</button>
             </span>
            </li>
 }
